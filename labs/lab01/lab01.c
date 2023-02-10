7db334bba700aff1173da79697005c8ae631fbbe
ujjayant-kadian <kadianu@tcd.ie>
2023-02-10 22:51:36 +0000

    Lab 01 - Complete

--- labs/lab01/lab01.c
+++ labs/lab01/lab01.c
@@ -1,11 +1,32 @@
-/**
- * @brief LAB #01 - TEMPLATE
- *        Main entry point for the code.
- * 
- * @return int      Returns exit-status zero on completion.
- */
+#include "pico/stdlib.h"
+
+uint LED_STATE = 0;
+
+void blink_led (uint LED_PIN, uint LED_DELAY) {
+    
+    
+    gpio_put(LED_PIN, LED_STATE);
+    sleep_ms(LED_DELAY);
+
+    LED_STATE = !LED_STATE;
+
+}
 int main() {
 
-    // Returning zero indicates everything went okay.
+    // Specify the PIN number and sleep delay
+    const uint LED_PIN   =  25;
+    const uint LED_DELAY = 500;
+
+    // Setup the LED pin as an output.
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    // Do forever...
+    while (true) {
+        blink_led(LED_PIN, LED_DELAY);
+    }
+
+    // Should never get here due to infinite while-loop.
     return 0;
+
 }
